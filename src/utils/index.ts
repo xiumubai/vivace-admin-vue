1e7a950b4e23fc2fe390dbcd49c6c22096cbd4ed
苑红林 <18203641939@163.com>
2023-08-01 13:20:34 +0800

    feat: 类型校验

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -76,8 +76,8 @@ export function downloadBase64file(base64: any, fileName = '文件') {
   const arr = base64.split(',')
   const mime = arr[0].match(/:(.*?);/)[1]
   const bstr = window.atob(arr[1])
+  let n = bstr.length
   const u8arr = new Uint8Array(n)
-  const n = bstr.length
   while (n--) {
     u8arr[n] = bstr.charCodeAt(n)
   }
